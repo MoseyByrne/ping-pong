c576bf8787ad01d448c325099b2f57a07c34f3c0
MoseyByrne <mosey.b.byrne@gmail.com>
2022-07-11 12:05:18 -0700

    intial files and start og front end
    Co-authored-by: jpalaoro197 <jpalaoro197@users.noreply.github.com>

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using System;
+using Models;
+
+namespace PingPong
+{
+  public class Program
+{
+  public static void Main()
+  {
+     Console.WriteLine("enter a positive number");
+     string stringUserNumber = Console.ReadLine();
+     int userNumber = int.Parse(stringUserNumber);
+
+     Console.WriteLine("here is your pingpong list");
+     
+    
+  }
+}
+}
